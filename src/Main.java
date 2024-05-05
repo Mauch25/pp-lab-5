951dfd12105b62776339636a0e68cfe3b2b78bf5
Mauch25 <chrzanowskimateusz25@gmail.com>
2024-05-05 14:41:10 +0200

    Zad

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.interfaces.Messenger;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("John", 30);
+            Person person = new Person("Albert", 30);
             System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
         } catch (InvalidAgeException e) {
             System.out.println("Invalid age exception: " + e.getMessage());
         }
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("Hello! This is a test message.");
     }
 }
 
